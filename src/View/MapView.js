796033961987ef9fd5612fd4c321830b47a36bbf
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2019-05-28 13:02:24 +0200

    Betere mapview en debug in browser ipv een bundle

--- src/View/MapView.js
+++ src/View/MapView.js
@@ -1,26 +1,48 @@
 export default class MapView {
 
+    getMousePos(c, evt) {
+        let rect = c.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
+
+    handleClick(e) {
+        let pos = getMousePos(this.canvas,e);
+        posx = pos.x;
+        posy = pos.y;
+
+        alert(posx + " " + posy);
+    }
+
+    constructor() {
+        //Zoo width and hieght
+        this.bw = 800;
+        this.bh = 800;
+
+        //Padding
+        this.p = 10;
+
+        this.canvas = document.getElementById("canvas");
+        this.context = canvas.getContext("2d");
+
+        canvas.addEventListener('click', handleClick);
+    }
+
     drawBoard() {
-        // Box width
-        let bw = 800;
-        // Box height
-        let bh = 800;
-        // Padding
-        let p = 10;
 
-        let canvas = document.getElementById("canvas");
-        let context = canvas.getContext("2d");
-
-        for (var x = 0; x <= bw; x += 80) {
-            context.moveTo(0.5 + x + p, p);
-            context.lineTo(0.5 + x + p, bh + p);
+        for (var x = 0; x <= this.bw; x += 80) {
+            this.context.moveTo(0.5 + x + this.p, this.p);
+            this.context.lineTo(0.5 + x + this.p, this.bh + this.p);
         }
     
-        for (var x = 0; x <= bh; x += 80) {
-            context.moveTo(p, 0.5 + x + p);
-            context.lineTo(bw + p, 0.5 + x + p);
+        for (var x = 0; x <= this.bh; x += 80) {
+            this.context.moveTo(this.p, 0.5 + x + this.p);
+            this.context.lineTo(this.bw + this.p, 0.5 + x + this.p);
         }
-        context.strokeStyle = "black";
-        context.stroke();
+        this.context.strokeStyle = "black";
+        this.context.stroke();
     }
+
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/app.js',
+  devtool: 'eval-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
