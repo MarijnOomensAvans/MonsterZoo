9d3faedfcc459bf83d9dc624c7d97fd761acb577
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2019-05-28 13:11:26 +0200

    Mouseclick op canvas

--- src/View/MapView.js
+++ src/View/MapView.js
@@ -1,5 +1,34 @@
 export default class MapView {
 
+    constructor() {
+        //Zoo width and hieght
+        this.bw = 800;
+        this.bh = 800;
+        
+        //Padding
+        this.p = 10;
+        
+        this.canvas = document.getElementById("canvas");
+        this.context = canvas.getContext("2d");
+        
+        this.canvas.addEventListener('click', handleClick);
+    }
+    
+    drawBoard() {
+        
+        for (var x = 0; x <= this.bw; x += 80) {
+            this.context.moveTo(0.5 + x + this.p, this.p);
+            this.context.lineTo(0.5 + x + this.p, this.bh + this.p);
+        }
+        
+        for (var x = 0; x <= this.bh; x += 80) {
+            this.context.moveTo(this.p, 0.5 + x + this.p);
+            this.context.lineTo(this.bw + this.p, 0.5 + x + this.p);
+        }
+        this.context.strokeStyle = "black";
+        this.context.stroke();
+    }
+
     getMousePos(c, evt) {
         let rect = c.getBoundingClientRect();
         return {
@@ -15,34 +44,5 @@ export default class MapView {
 
         alert(posx + " " + posy);
     }
-
-    constructor() {
-        //Zoo width and hieght
-        this.bw = 800;
-        this.bh = 800;
-
-        //Padding
-        this.p = 10;
-
-        this.canvas = document.getElementById("canvas");
-        this.context = canvas.getContext("2d");
-
-        canvas.addEventListener('click', handleClick);
-    }
-
-    drawBoard() {
-
-        for (var x = 0; x <= this.bw; x += 80) {
-            this.context.moveTo(0.5 + x + this.p, this.p);
-            this.context.lineTo(0.5 + x + this.p, this.bh + this.p);
-        }
     
-        for (var x = 0; x <= this.bh; x += 80) {
-            this.context.moveTo(this.p, 0.5 + x + this.p);
-            this.context.lineTo(this.bw + this.p, 0.5 + x + this.p);
-        }
-        this.context.strokeStyle = "black";
-        this.context.stroke();
-    }
-
 }
