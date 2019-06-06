ce7c52385ebf8959d00fe0aaac7286f7a0794e2d
MarijnOomensAvans <38461006+MarijnOomensAvans@users.noreply.github.com>
2019-06-06 16:08:35 +0200

    JSONgrid en begin fetch

--- src/View/MapView.js
+++ src/View/MapView.js
@@ -1,33 +1,37 @@
 export default class MapView {
+  constructor() {
+    fetch("http://localhost:8125/src/data/grid")
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        console.log(JSON.stringify(myJson));
+      });
+    this.map = document.getElementById("map");
+    this.zw = 10;
+    this.zh = 10;
+  }
 
-    constructor() {
-        this.map = document.getElementById('map');
-        this.zw = 10;
-        this.zh = 10;
+  drawBoard() {
+    this.table = document.createElement("table");
+    this.table.setAttribute("id", "grid");
+
+    for (let i = 0; i < this.zw; i++) {
+      this.tr = document.createElement("tr");
+      for (let y = 0; y < this.zh; y++) {
+        this.td = document.createElement("td");
+        this.tr.appendChild(this.td);
+      }
+      this.table.appendChild(this.tr);
     }
-    
-    drawBoard() {
-      this.table = document.createElement('table');
-      this.table.setAttribute('id','grid')
+    this.map.appendChild(this.table);
 
-        for (let i = 0; i < this.zw; i++) {
-            this.tr = document.createElement('tr');
-            for (let y = 0; y < this.zh; y++) {
-                this.td = document.createElement('td');
-                this.tr.appendChild(this.td);
-            }
-            this.table.appendChild(this.tr);
-            }
-            this.map.appendChild(this.table);
-
-
-        for (let i = 0; i < this.table.rows.length; i++) {
-            for (let j = 0; j < this.table.rows[i].cells.length; j++) {
-                this.table.rows[i].cells[j].onclick = function(i, j) {
-                    console.log(i + ":" + j)
-                }.bind(null, i, j)
-            }
-        }
+    for (let i = 0; i < this.table.rows.length; i++) {
+      for (let j = 0; j < this.table.rows[i].cells.length; j++) {
+        this.table.rows[i].cells[j].onclick = function(i, j) {
+          console.log(i + ":" + j);
+        }.bind(null, i, j);
+      }
     }
-    
+  }
 }
