21f849c9cf6120b1ff54acab937e0d73a7491b86
Marijn <mdoomens@avans.nl>
2019-07-17 16:05:20 +0200

    Object assign monster

--- src/Model/Monster.js
+++ src/Model/Monster.js
@@ -1,19 +1,7 @@
 export class Monster{
-    // MonsterData exists of element, name, arms, type_arms, legs, eyes, skintype, ability to fly, ability to swim,color ,map, coördinaten
-    constructor(element, name, arms, type_arms, legs, eyes, skintype, fly, swim, colour, map, x, y){
-        this.element = element;
-        this.name = name;
-        this.arms = arms;
-        this.type_arms = type_arms;
-        this.legs = legs;
-        this.eyes = eyes;
-        this.skintype = skintype;
-        this.fly = fly;
-        this.swim = swim;
-        this.map = map;
-        this.colour = colour;
-        this.x = x;
-        this.y = y;
+    // MonsterData exists of element, name, arms, type_arms, legs, eyes, skintype, ability to fly, ability to swim, colour ,map, x coordinate, y coordinate
+    constructor(monsterdata){
+        Object.assign(this, monsterdata);
     }
 }
 
